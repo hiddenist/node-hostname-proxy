2329c78ca67dc101302036b946bd940dc729b4e4
Nikki DelRosso <nikk3r@gmail.com>
2011-12-18 22:01:52 -0800

    Moved info to a readme file

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-/*
- * Runs a master node process to redirect hostnames to the correct node process
- * port using http-proxy. Think something like Apache virtual hosts.
- *
- * The data is populated from the config.js file, which expects a "hostnames" 
- * object with keys as the hostname, and data as the port.  The config file can
- * also provide a "redirects" object to define which hostnames should redirect
- * other hostnames (for example, www to non-www).
- *
- * See the config.sample.js file for a concrete example.
- *
- */
-
 var http = require('http'),
 	httpProxy = require('http-proxy'),
 	config = require('./config');
